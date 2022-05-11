9e409b029f140314d473aa8341e669b95ef22aa6
misaeljzg <misaeljzg@gmail.com>
2022-05-11 18:59:16 -0500

    created initial layout for landing page

---
+++ src/components/category-directory/category-directory.component.jsx
@@ -0,0 +1,16 @@
+import './category-directory.styles.scss'
+
+import CategoryItem from '../category-item/category-item.component'
+import './category-directory.styles.scss'
+
+const CategoryDirectory = ({categories}) => {
+    return (
+        <div className='category-directory-container'>
+            {categories.map((category) => (
+                <CategoryItem key={category.id} category={category} />
+            ))}
+        </div>
+    )
+}
+
+export default CategoryDirectory;
